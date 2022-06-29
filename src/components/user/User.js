f15a20f350814c48bd2048ab2d0a414832acf097
OlegBilko <bilko.oleg@gmail.com>
2022-06-30 00:35:11 +0300

    for lection HW_2_1 complette + button

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -2,15 +2,20 @@ import React from "react";
 
 import './User.css'
 
-export default function User({user_item}) {
+export default function User({user_item,myUser}) {
 
-return (
-<div>
+    return (
 
-    <div className={'us'}>
-        {user_item.id} . {user_item.name} - {user_item.email}.
-    </div>
 
-</div>
-);
-}
+            <div className={'us'}>
+                {user_item.id} . {user_item.name}.
+
+                <button onClick={() =>{
+                    myUser(user_item);
+                }}>User details </button>
+
+                    </div>
+
+
+                    );
+                }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -5,9 +5,14 @@ import User from "../user/User";
 import './Users.css'
 
 
+
 const Users = () => {
 
+
     const [users, setUsers] = useState([]);
+    const [user, setUser] = useState({});
+
+
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(value => value.json())
@@ -16,11 +21,29 @@ const Users = () => {
             });
     }, [])
 
+    const myUser = (item) => {
+        setUser(item);
+    };
+
     return (
         <div>
 
             <h2>Users component page</h2>
-            {users.map(user => <User user_item={user}/>)}
+            {users.map((user, index) =>
+                <User
+                    key={index}
+                    user_item={user}
+                    myUser={myUser}
+                />)}
+
+            {
+                user.id && <div className={'details'}>
+                    {user.id}.
+                    {user.name}.<br/>
+                    Email:{user.email}..<br/>
+                    Website:{user.website}
+                </div>
+            }
 
         </div>
     );
